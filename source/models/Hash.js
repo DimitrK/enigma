25c22b6961253b205102d277d5090b1f23a37ef3
DimitrK <jim.feedback@gmail.com>
2014-05-28 20:13:20 +0300

    Hash updates its bound encoding functions on Value changes

--- source/models/Hash.js
+++ source/models/Hash.js
@@ -1,21 +1,27 @@
 var enyo = enyo;
-enyo.kind({
-    name: "enigma.Hash",
-    kind: enyo.Object,
-    published: {
-        value: ""
-    },
-    //* @protected
-    constructor: function(hash) {
-        this.value = hash;
+(function(){
+    var propStartsWith_to_filter = function(inArg) {
+        return inArg.indexOf("to") === 0;
+    };
+    enyo.kind({
+        name: "enigma.Hash",
+        kind: enyo.Object,
+        published: {
+            value: ""
+        },
+        //* @protected
+        constructor: function(hash) {
+            this.value = hash;
 
-        var propStartsWith_to_filter = function(inArg) {
-            return inArg.indexOf("to") === 0;
-        };
-        var bindings = [hash];
+            var bindings = [this.value];
 
-        var boundFns = enigma.utils.bindObjPropsWithFilter(enigma.encode, bindings, propStartsWith_to_filter);
-        
-        enyo.mixin(this, boundFns);
-    }
-});
+            var boundFns = enigma.utils.bindObjPropsWithFilter(enigma.encode, bindings, propStartsWith_to_filter);
+            
+            enyo.mixin(this, boundFns);
+        },
+        valueChanged: function() {
+            var boundFns = enigma.utils.bindObjPropsWithFilter(enigma.encode, [this.value], propStartsWith_to_filter);
+            enyo.mixin(this, boundFns);
+        }
+    });
+})();
